52abfd5a9c83f9361ec64a37c7c04b197ba0669b
jmsuh <jmsuh@jmsuh>
2022-08-30 22:00:36 +0900

    zzzz

--- models/User.js
+++ models/User.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        maxlength:50,
-        minlength:3,
+        maxlength:50
     },
     email:{
         type:String,
