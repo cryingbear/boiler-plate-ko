3efd27905388689aabfcec78ce6f6bf23f3d504a
cryingbear <sjm4500@naver.com>
2022-08-30 22:38:47 +0900

    Update key.js
    dd

--- config/key.js
+++ config/key.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod')
-}else if(process.env.NODE_ENV === 'production2'){
-    module.exports = require('./prod2')
+}else if(process.env.NODE_ENV === 'production3'){
+    module.exports = require('./prod3')
 }else{
     module.exports = require('./dev')
 }
